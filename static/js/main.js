9100a358ac1b14e895ca20574bbdf965ccc298e0
Daniel Schramm <daniel.schramm@emvi.com>
2024-03-14 18:56:27 +0100

    Added hero section.

---
+++ static/js/main.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Scroll animation
+    
+    sal({
+        threshold: .2,
+    });
+
+    // Header
+
+    let header = document.getElementById("header")
+
+    function setHeaderClass() {
+        if(header) {
+            if(window.scrollY > 0) {
+                header.classList.add("small");
+            } else {
+                header.classList.remove("small");
+            }
+        }
+    }
+
+    setHeaderClass();
+
+    window.addEventListener("scroll", () => {
+        setHeaderClass();
+    });
+
+    let headerLogo = document.getElementById("headerLogoLink");
+    let headerLogoLink = headerLogo.getAttribute("href");
+
+    if(headerLogo) {
+        headerLogo.addEventListener("click", e => {
+            e.preventDefault();
+            e.stopPropagation();
+
+            if(window.location.pathname === headerLogoLink) {
+                window.scrollTo({top: 0, behavior: "smooth"});
+            } else {
+                window.location.href = headerLogoLink;
+            }
+        });
+    }
+
+    // Dev
+
+    history.scrollRestoration = "manual"
+});
