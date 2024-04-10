9ec76f3b698db54dfb4ccd4f9ce578d5a2582aff
Daniel Schramm <daniel.schramm@emvi.com>
2024-04-10 22:29:16 +0200

    Added sign up, discourse login and more translations.

--- static/js/main.js
+++ static/js/main.js
@@ -5,6 +5,45 @@ document.addEventListener("DOMContentLoaded", () => {
         threshold: .1
     });
 
+    // Logged In
+
+    function menu() {
+        const showLoggedIn = document.getElementsByClassName("showLoggedIn");
+        const hideLoggedIn = document.getElementsByClassName("hideLoggedIn");
+
+        if(getCookie("access_token")) {
+            for(let node of showLoggedIn) {
+                node.style.display = "inline-flex";
+            }
+        }
+        else {
+            for(let node of hideLoggedIn) {
+                node.style.display = "inline-flex";
+            }
+        }
+    }
+
+    function getCookie(name) {
+        name += "=";
+        let ca = document.cookie.split(';');
+
+        for(let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+
+            while(c.charAt(0) === ' ') {
+                c = c.substring(1,c.length);
+            }
+
+            if(c.indexOf(name) === 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+
+        return null;
+    }
+
+    window.addEventListener("DOMContentLoaded", menu);
+
     // Header
 
     let header = document.getElementById("header")
